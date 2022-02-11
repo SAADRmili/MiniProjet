bdebbf844cee9760a3b37660c8330d7296ee38e3
SAADRmili <saadrmili45@gmail.com>
2022-02-11 18:08:03 +0100

    Add tests

--- MiniProjet.Test/SheardProductContextTest.cs
+++ MiniProjet.Test/SheardProductContextTest.cs
@@ -34,6 +34,60 @@ namespace MiniProjet.Test
             var count = fixture.Context.GetProducts().Count;
 
             Assert.Equal(1, count);
+           
+        }
+
+
+
+        [Fact]
+        public void RemoveProductTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var product = new Product
+                {
+                    ProductName = "Product"+i,
+                    ProductPrice = 500,
+                    Brand = new Brand
+                    {
+                        BrandName = "Brand"+i
+                    }
+                };
+                fixture.Context.AddNewProduct(product);
+            }
+
+            var products = fixture.Context.GetProducts();
+
+            var productRemoved = products[2];
+
+            fixture.Context.RemoveProduct(productRemoved.ProductId);
+
+
+            Assert.DoesNotContain(products, p => p.ProductId == productRemoved.ProductId);
+        }
+
+
+        [Fact]
+
+        public void AddDiscountTest()
+        {
+            var product = new Product
+            {
+                ProductName = "Product1",
+                ProductPrice = 500,
+                Brand = new Brand
+                {
+                    BrandName = "Brand1"
+                }
+            };
+            fixture.Context.AddNewProduct(product);
+
+            fixture.Context.AddDiscount(0.7, product);
+
+
+            Assert.NotNull(product);
+            Assert.True(product.PromoPrice != null);
+            Assert.Equal(150, product.PromoPrice);
         }
     }
 }
